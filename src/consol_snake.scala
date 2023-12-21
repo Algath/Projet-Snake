dd12989b3eb43e723387320a4a7aab6fa18c4983
Algath <119356079+Algath@users.noreply.github.com>
2023-12-21 11:24:58 +0100

    position original de la tête

--- src/consol_snake.scala
+++ src/consol_snake.scala
@@ -1,17 +1,21 @@
 object consol_snake extends App {
  class Tableau(height: Int  = 8, width: Int = 8) {
 
-   var grid : Array[Array[Int]] = Array.ofDim(height,width)
-    grid.fill(height,width){0}
-   def createGrid() : Unit = {
+   var grid : Array[Array[Int]] = Array.fill(height,width){0}
 
+   def createGrid() : Unit = {}
 
-     }
+   var headSnake = 1
+   var taille = 3
 
+   var XInitialHead: Int = 3
+     //(math.random()*width).toInt
+//   var yInitialHead: Int = (math.random()*height).toInt
 
+   grid(XInitialHead)(1) = headSnake
 
+   for(i <- taille){
+     grid(3-i)(1) =
    }
-
-
  }
 }
