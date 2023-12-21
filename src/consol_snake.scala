0a9417c7aa02b206caa92470122951a4ce404b14
florian.sauzeat <florian.sauzeat@hes-so.ch>
2023-12-21 11:50:09 +0100

    SUITE (2)

--- src/consol_snake.scala
+++ src/consol_snake.scala
@@ -1,21 +1,56 @@
+import java.util.Scanner
+
+
 object consol_snake extends App {
- class Tableau(height: Int  = 8, width: Int = 8) {
+  class Tableau(height: Int = 8, width: Int = 8) {
 
-   var grid : Array[Array[Int]] = Array.fill(height,width){0}
+    var grid: Array[Array[Int]] = Array.fill(height, width) {
+      0
+    }
 
-   def createGrid() : Unit = {}
+    def createGrid(): Unit = {}
 
-   var headSnake = 1
-   var taille = 3
+    var headSnake = 1
+    var taille = 3
 
-   var XInitialHead: Int = 3
-     //(math.random()*width).toInt
-//   var yInitialHead: Int = (math.random()*height).toInt
+    var XInitialHead: Int = 3
+    //(math.random()*width).toInt
+    var yInitialHead: Int = 1
 
-   grid(XInitialHead)(1) = headSnake
+    grid(XInitialHead)(yInitialHead) = headSnake
 
-   for(i <- taille){
-     grid(3-i)(1) =
-   }
- }
+    for (i <- 1 until taille) {
+      grid(XInitialHead - i)(yInitialHead) = i + 1
+    }
+    //define apple
+    var apple: Int = -1
+
+    def printGrid(): Unit = {
+      var text: String = ""
+      for (i <- grid.indices) {
+        for (j <- grid(i).indices) {
+          text += s"${grid(i)(j)}"
+        }
+        text += "\n"
+      }
+println(text)
+    }
+
+  }
+
+  class Game(){
+
+    var keyboard : Scanner = new Scanner(System.in)
+    keyboard
+
+
+
+
+
+
+  }
+
+  var x : Tableau = new Tableau()
+  x.printGrid()
+
 }
