0c9adc6f04f6499f646cc97a5b47a821391b2e65
Algath <119356079+Algath@users.noreply.github.com>
2023-12-31 15:44:21 +0100

    déplacement du serpent fait, reste plus qu'a le rendre fluide

--- src/consol_snake.scala
+++ src/consol_snake.scala
@@ -1,16 +1,13 @@
 import java.util.Scanner
 
-
 object consol_snake extends App {
-  class Tableau(height: Int = 8, width: Int = 8) {
+  class Tableau(width: Int = 8, height: Int = 8) {
 
-    var grid: Array[Array[Int]] = Array.fill(height, width) {
-      0
-    }
+    var grid: Array[Array[Int]] = Array.fill(width, height) (0)
 
-    def createGrid(): Unit = {}
+    //def createGrid(): Unit = {}
 
-    def postionSnake(XInitialHead: Int, YInitialHead: Int, taille: Int): Unit = {
+    def postionSnake(XInitialHead: Int, YInitialHead: Int, taille: Int, deplacementx : Int = 1, deplacementy : Int): Unit = {
       var headSnake = 1
       var apple: Int = -1
 
@@ -18,20 +15,21 @@ object consol_snake extends App {
       for (i <- grid.indices) {
         for (j <- grid(i).indices) {
           if (grid(i)(j) > 0) {
-            grid(i)(j) = 0
+            grid(i)(j) -= 1
           }
         }
       }
 
-      grid(XInitialHead)(YInitialHead) = headSnake
+      if (XInitialHead >= 0 && XInitialHead < width && YInitialHead >= 0 && YInitialHead < height) {
+        grid(XInitialHead)(YInitialHead) = headSnake
+      }
 
-      for (i <- 1 until taille) {
-        if (XInitialHead - i >= 0 && XInitialHead - i < height) {
-          grid(XInitialHead - i)(YInitialHead) = i + 1
-          //revoir mouvement du corps
+
+      //gestion du mouvement du corps
+      for (i <- 2 to  taille) {
+        if (XInitialHead + i - deplacementx >= 0 && XInitialHead + i - deplacementx < width && YInitialHead >= 0 && YInitialHead < height) {
+          grid(XInitialHead + i - deplacementx)(YInitialHead) = i // Corps du serpent
         }
-
-
       }
     }
 
@@ -45,49 +43,48 @@ object consol_snake extends App {
       }
       println(text)
     }
+  }
+  class Game() {
+    var keyboard: Scanner = new Scanner(System.in)
+    var snake: Tableau = new Tableau()
+    snake.postionSnake(3, 1, 3,1,0)
+    snake.printGrid()
+    var XInitialHead = 3
+    var YInitialHead = 1
+    var taille = 3
+    var deplacementx = 1
+    var deplacementy = 1
 
 
-    class Game() {
-      var keyboard: Scanner = new Scanner(System.in)
-      var snake: Tableau = new Tableau()
-      snake.postionSnake(3, 1, 3)
-      snake.printGrid()
-      var XInitialHead = 3
-      var YInitialHead = 1
-      var taille = 3
-
-
-      def startGame() {
-        while (true) {
-          val input = keyboard.nextLine()
-          if (input.length == 1) {
-            val direction = input.charAt(0)
-            direction match {
-              case 'w' | 'W' =>
-                XInitialHead += 1 // Modifier la coordonnée Y vers le haut
-                snake.postionSnake(XInitialHead, YInitialHead, taille)
-              case 'a' | 'A' =>
-                YInitialHead -= 1 // Modifier la coordonnée X vers la gauche
-                snake.postionSnake(XInitialHead, YInitialHead, taille)
-              case 's' | 'S' =>
-                XInitialHead -= 1 // Modifier la coordonnée Y vers le bas
-                snake.postionSnake(XInitialHead, YInitialHead, taille)
-              case 'd' | 'D' =>
-                YInitialHead += 1 // Modifier la coordonnée X vers la droite
-                snake.postionSnake(XInitialHead, YInitialHead, taille)
-              case _ => println("Commande invalide")
-            }
-            println(s"Position du personnage : (${XInitialHead}, ${YInitialHead})")
-            snake.printGrid()
-          } else {
-            println("Commande invalide")
+    def startGame() {
+      while (true) {
+        val input = keyboard.nextLine()
+        if (input.length == 1) {
+          val direction = input.charAt(0)
+          direction match {
+            case 'w' | 'W' =>
+              XInitialHead -= 1 // Modifier la coordonnée X vers le haut
+              snake.postionSnake(XInitialHead, YInitialHead, taille,deplacementx, deplacementy)
+            case 'a' | 'A' =>
+              YInitialHead -= 1 // Modifier la coordonnée Y vers la gauche
+              snake.postionSnake(XInitialHead, YInitialHead, taille,deplacementx, deplacementy)
+            case 's' | 'S' =>
+              XInitialHead += 1 // Modifier la coordonnée X vers le bas
+              snake.postionSnake(XInitialHead, YInitialHead, taille, deplacementx, deplacementy)
+            case 'd' | 'D' =>
+              YInitialHead += 1 // Modifier la coordonnée Y vers la droite
+              deplacementx += 1
+              snake.postionSnake(XInitialHead, YInitialHead, taille,deplacementx, deplacementy)
+            case _ => println("Commande invalide")
           }
+          println(s"Position du personnage : (${XInitialHead}, ${YInitialHead})")
+          snake.printGrid()
+        } else {
+          println("Commande invalide")
         }
       }
     }
-
-    var play: Game = new Game()
-    play.startGame()
-
   }
+  var play: Game = new Game()
+  play.startGame()
 }
